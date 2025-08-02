b51103a070891c34d664fbfdc8d9a6d34be44191
yousefsobhy12 <yousefsobhy124@gmail.com>
2025-08-02 16:29:41 +0300

    feat(home): implement Home repository feat(utils): add AppServices with GET method using Dio

---
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplementation implements HomeRepo {
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBook() {
+    // TODO: implement fetchFeaturedBook
+    throw UnimplementedError();
+  }
+}
