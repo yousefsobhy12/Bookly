60f58071526ffd20c3a19c492e2e8bf1d87e7b17
yousefsobhy12 <yousefsobhy124@gmail.com>
2025-08-02 15:10:59 +0300

    feat(core): create Failures class for error handling feat(home): create Home repository

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/model/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBook();
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
+}
