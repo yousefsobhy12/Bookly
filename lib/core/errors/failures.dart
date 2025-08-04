97b777eca6b72e62dff75655db613791650f40e4
yousefsobhy12 <yousefsobhy124@gmail.com>
2025-08-04 19:17:23 +0300

    feat: add ServerFailure class and complete fetchNewestBooks & fetchFeaturedBooks implementations

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,1 +1,41 @@
-abstract class Failures {}
+import 'package:dio/dio.dart';
+
+abstract class Failures {
+  final String message;
+
+  Failures(this.message);
+}
+
+class ServerFailure extends Failures {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioError(DioException error) {
+    switch (error.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout');
+      case DioExceptionType.badResponse:
+        return ServerFailure('Bad Response: ${error.response?.statusCode}');
+      case DioExceptionType.cancel:
+        return ServerFailure('Request Cancelled');
+      case DioExceptionType.unknown:
+      default:
+        return ServerFailure('Unknown Error: ${error.message}');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return ServerFailure('Your request was not found, please try later');
+    } else if (statusCode == 500) {
+      return ServerFailure('There is a problem with server, please try later');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else {
+      return ServerFailure('There was an error , please try again');
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -1,18 +1,48 @@
 import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/core/utils/app_service.dart';
 import 'package:bookly/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImplementation implements HomeRepo {
+  final AppService appService;
+
+  HomeRepoImplementation(this.appService);
   @override
-  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await appService.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on DioException catch (error) {
+      return left(ServerFailure.fromDioError(error));
+    } catch (error) {
+      return left(ServerFailure('An unexpected error occurred: $error'));
+    }
   }
 
   @override
-  Future<Either<Failures, List<BookModel>>> fetchFeaturedBook() {
-    // TODO: implement fetchFeaturedBook
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBook() async {
+    try {
+      var data = await appService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on DioException catch (error) {
+      return left(ServerFailure.fromDioError(error));
+    } catch (error) {
+      return left(ServerFailure('An unexpected error occurred: $error'));
+    }
   }
 }
